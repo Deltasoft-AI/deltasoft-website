1a1bb903c218b2b48bec52b59ad54dc1460b1d33
JSman225 <84781179+JSman225@users.noreply.github.com>
2023-12-01 15:11:10 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -61,7 +61,7 @@ export default function Home() {
       <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
         <div className="mx-auto flex max-w-xs flex-col gap-y-4">
           <dt className="text-base leading-7 text-gray-600">Community Members</dt>
-          <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">54+</dd>
+          <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">60+</dd>
         </div>
         <div className="mx-auto flex max-w-xs flex-col gap-y-4">
           <dt className="text-base leading-7 text-gray-600"><span className="font-semibold leading-6 text-indigo-600">Deltasoft
